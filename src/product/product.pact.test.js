57d89da9e0c9a240ef7fd013f1845c66915483a7
Matt Fellows <53900+mefellows@users.noreply.github.com>
2023-09-06 22:34:59 +1000

    chore: update selectors

--- src/product/product.pact.test.js
+++ src/product/product.pact.test.js
@@ -31,7 +31,7 @@ describe("Pact Verification", () => {
     const fetchPactsDynamicallyOpts = {
       provider: "pactflow-example-provider",
       //consumerVersionTag: ['master', 'prod'], //the old way of specifying which pacts to verify
-      consumerVersionSelectors: [{ tag: 'master', latest: true }, { deployed: true } ], // the new way of specifying which pacts to verify
+      consumerVersionSelectors: [{ tag: 'master', latest: true }, { mainBranch: true }, { deployedOrReleased: true } ], // the new way of specifying which pacts to verify
       pactBrokerUrl: process.env.PACT_BROKER_BASE_URL,
       publishVerificationResult: process.env.PACT_BROKER_PUBLISH_VERIFICATION_RESULTS ? true : false,
       enablePending: false,
